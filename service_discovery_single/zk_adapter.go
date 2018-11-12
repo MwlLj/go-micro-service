a2d49f90188248c074cf2476f55b3be6cae38cd1
MwlLj <731025894@qq.com>
2018-11-13 00:02:58 +0800

    connect zookeeper server

--- service_discovery_single/zk_adapter.go
+++ service_discovery_single/zk_adapter.go
@@ -3,11 +3,14 @@ package service_discovery_single
 import (
 	"fmt"
 	"github.com/samuel/go-zookeeper/zk"
+	"strconv"
+	"strings"
 )
 
 var _ = fmt.Println
 
 type CZkAdapter struct {
+	m_conn *zk.Conn
 }
 
 func (this *CZkAdapter) Init(property *CInitProperty) error {
@@ -15,5 +18,11 @@ func (this *CZkAdapter) Init(property *CInitProperty) error {
 }
 
 func (this *CZkAdapter) Connect(property *CConnectProperty) error {
+	host := strings.Join([]string{property.ZkServerHost, strconv.FormatInt(property.ZkServerPort, 10)}, ":")
+	this.m_conn, connChan, err = zk.Connect(host, property.ConnTimeoutS)
+	if err != nil {
+		fmt.Println("connect zookeeper server error")
+		return err
+	}
 	return nil
 }
