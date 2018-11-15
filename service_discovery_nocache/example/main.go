84d7deef27617a150487852dc56c73cb4622fa70
MwlLj <731025894@qq.com>
2018-11-15 18:32:38 +0800

    example中新增 serverUniqueCode 字段

--- service_discovery_nocache/example/main.go
+++ service_discovery_nocache/example/main.go
@@ -10,12 +10,13 @@ func main() {
 	var conns []s.CConnectProperty
 	conns = append(conns, s.CConnectProperty{ServerHost: "127.0.0.1", ServerPort: 2182, ServiceId: "server_1"})
 	sds := s.New(&s.CInitProperty{
-		PathPrefix:   "mico-service",
-		ServerMode:   s.ServerModeZookeeper,
-		ServerName:   "testserver",
-		NodePayload:  `{"ip":"192.168.9.15","port":50000}`,
-		Conns:        conns,
-		ConnTimeoutS: 10})
+		PathPrefix:       "mico-service",
+		ServerMode:       s.ServerModeZookeeper,
+		ServerName:       "testserver",
+		ServerUniqueCode: "0B5398177EBA429898F68AF13909920E",
+		NodePayload:      `{"ip":"192.168.9.15","port":50000}`,
+		Conns:            conns,
+		ConnTimeoutS:     10})
 	err := sds.Connect()
 	if err != nil {
 		fmt.Println(err)
