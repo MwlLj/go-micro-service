1e83ed422975ab0daebec6f1d1e2d8f371ef7774
MwlLj <731025894@qq.com>
2019-01-20 23:38:32 +0800

    新增test

---
+++ load_balance/example/mqtt/configs/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/MwlLj/mqtt_comm"
+)
+
+var _ = fmt.Println
+
+type CServer struct {
+	m_mqttComm mqtt_comm.CMqttComm
+}
+
+type CAddServerInfoHandler struct {
+}
+
+func (this *CAddServerInfoHandler) Handle(topic *string, action *string, request *string, qos int, mc mqtt_comm.CMqttComm, user interface{}) (*string, error) {
+	return nil, nil
+}
+
+func (this *CServer) Start() {
+	this.m_mqttComm = mqtt_comm.NewMqttComm("configs", "1.0", 0)
+	this.m_mqttComm.SetMessageBus("localhost", 51885, "", "")
+	this.m_mqttComm.Subscribe(mqtt_comm.POST, "1.0/configs/serverinfo", 0, &CAddServerInfoHandler{}, this)
+	this.m_mqttComm.Connect(true)
+}
+
+func main() {
+	server := CServer{}
+	server.Start()
+}
