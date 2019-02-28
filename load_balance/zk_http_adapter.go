1aa496d94846e1798bb63647e620ba712d594635
MwlLj <731025894@qq.com>
2019-03-01 00:18:35 +0800

    实现http的转发操作

--- load_balance/zk_http_adapter.go
+++ load_balance/zk_http_adapter.go
@@ -7,7 +7,9 @@ import (
 	"fmt"
 	proto "github.com/MwlLj/go-micro-service/common_proto"
 	"github.com/samuel/go-zookeeper/zk"
+	"log"
 	"net/http"
+	"net/http/httputil"
 	"strconv"
 	"strings"
 	"sync"
@@ -25,31 +27,32 @@ var _ = sync.NewCond
 
 type CZkHttpAdapter struct {
 	CZkAdapter
-	m_configReader *CConfigReader
+	m_normalNodeAlgorithm INormalNodeAlgorithm
+	m_configReader        *CConfigReader
 }
 
 func (this *CZkHttpAdapter) init(conns *[]proto.CConnectProperty, pathPrefix string, connTimeoutS int) (<-chan bool, error) {
 	return this.CZkAdapter.init(conns, pathPrefix, connTimeoutS)
 }
 
-func (this *CZkMqttAdapter) onMessage(w http.ResponseWriter, r *http.Request) {
+func (this *CZkHttpAdapter) onMessage(w http.ResponseWriter, r *http.Request) error {
 	top := r.URL.String()
 	ruleInfo, isFind, err := this.m_configReader.FindRuleInfoByTopic(&top)
 	if err != nil {
 		log.Println("find router rule by topic error, err: ", err)
-		return nil, err
+		return err
 	}
-	var buffer bytes.Buffer
+	var hostBuffer bytes.Buffer
 	if isFind == false {
 		log.Println("not find router rule by topic")
-		return nil, errors.New("rule is not match")
+		return errors.New("rule is not match")
 	} else {
 		var nodeData *proto.CNodeData = nil
 		if ruleInfo.IsMaster {
 			nodeData, err = this.GetMasterNode(ruleInfo.ObjServerName)
 			if err != nil {
 				fmt.Println(err)
-				return nil, err
+				return err
 			}
 		} else {
 			nodeData, err = this.m_normalNodeAlgorithm.Get(ruleInfo.ObjServerName, nil)
@@ -57,16 +60,22 @@ func (this *CZkMqttAdapter) onMessage(w http.ResponseWriter, r *http.Request) {
 				nodeData, err = this.GetMasterNode(ruleInfo.ObjServerName)
 				if err != nil {
 					fmt.Println(err)
-					return nil, err
+					return err
 				}
 			}
 		}
-		serverUuid := nodeData.ServerUniqueCode
-		buffer.WriteString(*top)
-		// buffer.WriteString("/")
-		buffer.WriteString(serverUuid)
+		hostBuffer.WriteString(nodeData.ServerIp)
+		hostBuffer.WriteString(":")
+		hostBuffer.WriteString(strconv.FormatInt(int64(nodeData.ServerPort), 10))
 	}
-	response, err := mqttComm.Send(*action, buffer.String(), *request, qos, this.m_transmitTimeoutS)
+	director := func(req *http.Request) {
+		req.URL.Scheme = "http"
+		req.URL.Host = hostBuffer.String()
+		req.URL.Path = top
+	}
+	proxy := &httputil.ReverseProxy{Director: director}
+	proxy.ServeHTTP(w, r)
+	return nil
 }
 
 func (this *CZkHttpAdapter) SetConfigInfo(info *CConfigInfo) error {
@@ -81,7 +90,21 @@ func (this *CZkHttpAdapter) SetConfigInfo(info *CConfigInfo) error {
 	return err
 }
 
+func (this *CZkHttpAdapter) SetNormalNodeAlgorithm(algorithm string) error {
+	this.m_normalNodeAlgorithm = this.GetNormalNodeAlgorithm(algorithm)
+	if this.m_normalNodeAlgorithm == nil {
+		return errors.New("not support")
+	}
+	return nil
+}
+
 func (this *CZkHttpAdapter) Run(data interface{}) error {
+	if this.m_configReader == nil {
+		return errors.New("please call SetConfigFilePath")
+	}
+	if this.m_normalNodeAlgorithm == nil {
+		return errors.New("please call SetNormalNodeAlgorithm")
+	}
 	var err error = nil
 	defer func() {
 		if e := recover(); e != nil {
@@ -94,10 +117,20 @@ func (this *CZkHttpAdapter) Run(data interface{}) error {
 	addrBuffer.WriteString(serverInfo.Host)
 	addrBuffer.WriteString(":")
 	addrBuffer.WriteString(strconv.FormatInt(int64(serverInfo.Port), 10))
-	http.ListenAndServe(addrBuffer.String(), func(w http.ResponseWriter, r *http.Request) {
-		this.onMessage(w, r)
+	http.ListenAndServe(addrBuffer.String(), &CHttpMux{
+		m_handler: func(w http.ResponseWriter, r *http.Request) {
+			this.onMessage(w, r)
+		},
 	})
 	return err
+}
+
+type CHttpMux struct {
+	m_handler func(w http.ResponseWriter, r *http.Request)
+}
+
+func (this *CHttpMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	this.m_handler(w, r)
 }
 
 func (this *CZkHttpAdapter) GetNormalNodeAlgorithm(algorithm string) INormalNodeAlgorithm {
